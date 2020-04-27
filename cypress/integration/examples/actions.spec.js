2edf610ca54b7c26a5a71b051662597d9a646cfc
sainthkh <sainthkh@naver.com>
2020-04-27 16:14:25 +0900

    initial update

---
+++ cypress/integration/examples/actions.spec.js
@@ -0,0 +1,6 @@
+/// <reference types="cypress" />
+describe('Actions', function () {
+    it('.type() - type into a DOM element', function () {
+        cy.visit('https://google.com');
+    });
+});
---
+++ cypress/integration/examples/actions.spec.ts
@@ -0,0 +1,7 @@
+/// <reference types="cypress" />
+
+describe('Actions', () => {
+  it('.type() - type into a DOM element', () => {
+    cy.visit('https://google.com')
+  })
+})
